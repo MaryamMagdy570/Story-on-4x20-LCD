043a98f79367d13fed5c72d349f26b80ee50a1f1
Maryam Magdy <82731141+MaryamMagdy570@users.noreply.github.com>
2023-10-26 13:14:22 +0300

    Drivers

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 25, 2023
+ *      Author: Maryam Magdy
+ */
+
+#include <util/delay.h>
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+#include "CLCD_interface.h"
+
+
+void main(void)
+{
+
+
+}
